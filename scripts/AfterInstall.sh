87647ab7959a6019cfdb1ead0c85c2bd033055d4
Hang <hangzhang925@gmail.com>
2022-06-17 14:57:29 -0700

    try sudo su

--- scripts/AfterInstall.sh
+++ scripts/AfterInstall.sh
@@ -3,8 +3,7 @@
 ls /var/www/solidus/
 ls /var/www
 echo "wtf???? ${pwd}"
+sudo su
 cd /var/www/solidous
 /var/www/solidous/bundle install --path vendor/bundle
 bundle exec rake db:migrate
-bundle exec rake assets:clobber
-bundle exec rake assets:precompile
