c8f4b44778fb8fbe3f6a45c5aca83a22f5d30263
Hang <hangzhang925@gmail.com>
2022-06-17 14:57:44 -0700

    fix

--- scripts/AfterInstall.sh
+++ scripts/AfterInstall.sh
@@ -5,5 +5,5 @@ ls /var/www
 echo "wtf???? ${pwd}"
 sudo su
 cd /var/www/solidous
-/var/www/solidous/bundle install --path vendor/bundle
+bundle install --path vendor/bundle
 bundle exec rake db:migrate
